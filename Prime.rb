48810f56343a2147d270158e9d2cb76bfaf31802
michaelshamrell <michaelshamrell2001@gmail.com>
2022-04-20 18:54:52 -0700

    Cleaning up code.

--- Prime.rb
+++ Prime.rb
@@ -33,42 +33,4 @@ y4 = x.div(11)
     puts x.to_s + " is a prime number"
   end
 
-# x = gets.chomp.to_i
-# 1.upto(x) do |x|
-# z = x.fdiv(2)
-# y = x.div(2)
-# z1 = x.fdiv(3)
-# y1 = x.div(3)
-# z2 = x.fdiv(5)
-# y2 = x.div(5)
-# z3 = x.fdiv(7)
-# y3 = x.div(7)
-# z4 = x.fdiv(11)
-# y4 = x.div(11)
-# end
-# 1.upto(x) do |x|
-#   if x == 2
-#     puts x.to_s + " is a prime number"
-#   elsif z == y
-#     puts x.to_s + " is not a prime number"
-#   elsif z != y
-#     puts x.to_s + " is a prime number"
-#   elsif z1 == y1
-#     puts x.to_s + " is not a prime number"
-#   elsif z1 != y1
-#     puts x.to_s + " is a prime number"
-#   elsif z2 == y2
-#     puts x.to_s + " is not a prime number"
-#   elsif z2 != y2
-#     puts x.to_s + " is a prime number"
-#   elsif z3 == y3
-#     puts x.to_s + " is not a prime number"
-#   elsif z3 != y3
-#     puts x.to_s + " is a prime number"
-#   elsif z4 == y4
-#     puts x.to_s + " is not a prime number"
-#   elsif z4 != y4
-#     puts x.to_s + " is a prime number"
-#   end
-# end
 
--- primeMethod.rb
+++ primeMethod.rb
@@ -1,5 +1,5 @@
 require 'prime'
 x = gets.chomp.to_i
 1.upto(x) do |x|
-  puts x.to_s + " is a " + x.prime?.to_s + " prime number"
+  puts "#{x} is a #{x.prime?} prime number"
 end
